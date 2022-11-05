99676426f2450c8ffcbf33eba03f81f03e4a9cc2
Gökberk Ali Kulaç <gokberkkulac@gmail.com>
2022-11-05 13:51:29 +0300

    All the challenge changes done in development branch and merge with main branch now its time to push to Github

--- Project-1 Storm Viewer App/ViewController.swift
+++ Project-1 Storm Viewer App/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UITableViewController {
 
     var pictures = [String]()
+    var sortedPictureList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Storm Viewer App"
@@ -24,21 +26,26 @@ class ViewController: UITableViewController {
                 pictures.append(item)
             }
         }
+        
+        sortedPictureList = pictures.sorted()
+        print(sortedPictureList)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pictures.count
+        return sortedPictureList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Pictures",for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = sortedPictureList[indexPath.row]
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController{
-            vc.selectedImage = pictures[indexPath.row]
+            vc.selectedImage = sortedPictureList[indexPath.row]
+            vc.selectedIndex = indexPath.row
+            vc.totalImageNumber = sortedPictureList.count
             navigationController?.pushViewController(vc, animated: true)
         }
     }
